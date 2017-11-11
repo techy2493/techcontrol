c3ce6a0cfa1ff782d2175b28a2c450164dfafc97
Brandyn <brandynabel@gmail.com>
2017-11-11 14:29:34 -0600

    Allow multiple users, and make them configurable from a json file

--- app/web/authenticate/login.js
+++ app/web/authenticate/login.js
@@ -1,20 +1,21 @@
 //TODO: make an actual session and validate.
 var express = require('express');
 var cookie = require('cookie-parser');
+var _ = require('lodash')
 
 
 
 function login() {
     this.authenticate = function (req, res) {
-        console.log(req.body);
-        var users = {techy:"test"}
-        if (users[req.body['user']]) {
-            if (users[req.body['user']] = req.body['pass']){
-                res.cookie("tcon_authenticated", true)
-                res.cookie("tcon_sessionid", 1)
-                res.cookie("tcon_user", req.body['user'])
+        //Load Controller Config File
+        var fs = require('fs');
+        users = JSON.parse(fs.readFileSync('./users.json', 'utf8'))['userlist'];
+        var user = users.find(x=> x.username === req.body['user'])
+        if (user && user.password === req.body['pass']) {
+                res.cookie('tcon_authenticated', true)
+                res.cookie('tcon_sessionid', 1)
+                res.cookie('tcon_user', req.body['user'])
                 res.redirect('/authorized/console.html')
-            }
         } else {
             res.redirect('login.html')
         }   
